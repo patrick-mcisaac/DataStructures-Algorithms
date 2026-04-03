8ee806d3f26a717ff9c647d88bdd0104977089e9
Patmc <patrick.mcisaac.dev@gmail.com>
2026-04-02 21:05:41 -0500

    added the global cooling crisis class

--- Combine Algorithms/globalCoolingCrisis.js
+++ Combine Algorithms/globalCoolingCrisis.js
@@ -11,25 +11,51 @@ class WeatherPatternAnalyzer {
 
   addTemperatureReading(stationId, temperature) {
     // TODO
+    if(this.stations.has(stationId)){
+      if(temperature - this.stations.get(stationId).latestTemp >= 5){
+        this.significant.add(stationId)
+        this.stations.set(stationId, {latestTemp: temperature, count: this.stations.get(stationId).count + 1})
+        return
+      }
+    }
+    this.stations.set(stationId, {latestTemp: temperature, count:  1})
   }
 
   getSignificantChangeStations() {
     // TODO
-    return [];
+
+    return [...this.significant].sort((a,b) => a.localeCompare(b))
+   
   }
 
   getStationData(stationId) {
     // TODO
+    if(this.stations.has(stationId)){
+      return this.stations.get(stationId)
+    }
     return null;
   }
 
   getAllActiveStations() {
     // TODO
-    return new Set();
+    const result = new Set()
+    for(const [id, data] of this.stations){
+      result.add(id)
+    }
+    return result
   }
 
   getSystemSummary() {
     // TODO
-    return { stationCount: 0, significantCount: 0 };
+    return { stationCount: this.stations.size, significantCount: this.significant.size };
   }
 }
+
+const weather = new WeatherPatternAnalyzer()
+weather.addTemperatureReading("station1", 20)
+weather.addTemperatureReading("station1", 27)
+weather.addTemperatureReading("station2", 27)
+console.log(weather.getAllActiveStations())
+console.log(weather.getStationData('station1'))
+console.log(weather.getSystemSummary())
+console.log(weather.getSignificantChangeStations())
